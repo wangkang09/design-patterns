bf6a37540fa93b323e3bf7372307270dd966d650
wangkang09 <41175635+wangkang09@users.noreply.github.com>
2019-05-25 16:06:51 +0800

    添加 package-info 信息

---
+++ src/main/java/com/wangkang/design_patterns/工厂模式/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Created by wangkang.
+ *
+ * Listing 代码清单 原生代码 {@link com.wangkang.design_patterns.工厂模式.PizzaStoreRaw}
+ *
+ * 代码清单 简单工厂 {@link com.wangkang.design_patterns.工厂模式.SimplePizzaFactory}
+ *
+ * 代码清单 工厂模式 {@link com.wangkang.design_patterns.工厂模式.PizzaStoreFactory}
+ *
+ * 代码清单 抽象工厂模式 {@link com.wangkang.design_patterns.工厂模式.PizzaStoreAbstractFactory}
+ */
+package com.wangkang.design_patterns.工厂模式;
