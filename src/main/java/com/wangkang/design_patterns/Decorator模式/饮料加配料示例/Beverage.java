e8fbe02ca6a7c35b4194eea34a06f3473c4795ef
wangkang09 <41175635+wangkang09@users.noreply.github.com>
2019-06-12 10:56:06 +0800

    抽象组件中 getDescription 方法说明

--- src/main/java/com/wangkang/design_patterns/Decorator模式/饮料加配料示例/Beverage.java
+++ src/main/java/com/wangkang/design_patterns/Decorator模式/饮料加配料示例/Beverage.java
@@ -8,6 +8,7 @@ package com.wangkang.design_patterns.Decorator模式.饮料加配料示例;
  */
 public abstract class Beverage {
     protected String description = "Unknown Beverage";//需要被子类继承，所有不能定义为私有
+    //因为所有的具体组件都返回的是 description，所以不需要设置为抽象类
     public String getDescription() {
         return description;
     }
